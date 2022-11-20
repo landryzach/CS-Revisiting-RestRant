c994f6933c5cb59ca69f89a44d4b0b0ac989068c
Zachary Landry <landryzachary4@gmail.com>
2022-11-20 13:28:50 -0500

    Activity: Sign Up & Log In

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 const app = express();
 
+
 // Express Settings
 app.use(cors())
 app.use(express.static('public'))
@@ -17,6 +18,7 @@ app.use(express.urlencoded({ extended: true }))
 
 app.use('/places', require('./controllers/places'))
 app.use('/users', require('./controllers/users'))
+app.use('/authentication', require('./controllers/authentication'))
 
 // Listen for Connections
 app.listen(process.env.PORT, () => {
